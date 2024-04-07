3d7a3cbdfa4c50d9a9f5ed371e7757e49157ef6e
NedoNeo <denshkur@yandex.ru>
2024-04-07 13:40:37 +0300

    homework

---
+++ js/__tests__/Daemon.test.js
@@ -0,0 +1,14 @@
+import Daemon from '../Daemon';
+
+test('should check the class Daemon', () => {
+  const character = new Daemon('Player', 'Daemon');
+  const result = {
+    name: 'Player',
+    type: 'Daemon',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  };
+  expect(character).toMatchObject(result);
+});
---
+++ js/index.js
@@ -0,0 +1,15 @@
+import Character from "./Character";
+import Bowerman from "./Bowman";
+import Swordsman from "./Swordman";
+import Magician from "./Magician";
+import Daemon from "./Daemon";
+import Undead from "./Undead";
+import Zombie from "./Zombie";
+
+console.log(Character);
+console.log(Bowerman);
+console.log(Swordsman);
+console.log(Magician);
+console.log(Daemon);
+console.log(Undead);
+console.log(Zombie);
